126431d0f38ffcf5534ef40226c5914244c44f0a
wongky87 <wongky-wm19@student.tarc.edu.my>
2021-03-24 10:31:17 +0800

    commit

--- Game_Test/Player.cpp
+++ Game_Test/Player.cpp
@@ -13,7 +13,7 @@ Player::Player()
 	scaling = D3DXVECTOR3(1.0f, 1.0f, 1.0f);
 	movement = D3DXVECTOR3();
 	size = D3DXVECTOR3(22.0f, 36.0f, 1.0f);
-	size = D3DXVECTOR3(128.0f, 128.0f, 1.0f);
+	//size = D3DXVECTOR3(128.0f, 128.0f, 1.0f);
 	spriteCentre = D3DXVECTOR3(size.x / 2, size.y / 2, 0.0f);
 	position = D3DXVECTOR3(WIN_WIDTH/2, WIN_HEIGHT/2, 0);
 	mat = D3DMATRIX();
